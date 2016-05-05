0a64f87b6b2224910c3dd99bf4a256fa3b2fbb2f
hank-sunday <hank.sunday@gmail.com>
2016-05-05 22:26:14 +0800

    添加Greenplum的sb，修改dashboard的实现方式。

--- dashboard/phpmyadmin/config.inc.php
+++ dashboard/phpmyadmin/config.inc.php
@@ -31,7 +31,7 @@ if (isset($_ENV['PMA_ABSOLUTE_URI'])) {
 /* Figure out hosts */
 
 /* Fallback to default linked */
-$hosts = array('servicebrokershare.cctlkf28famk.rds.cn-north-1.amazonaws.com.cn');
+$hosts = array('mysqlshare.servicebroker.dataos.io');
 
 /* Set by environment */
 if (!empty($_ENV['PMA_HOST'])) {
